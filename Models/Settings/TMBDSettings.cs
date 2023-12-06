48c5622026f05ae9bb8f34aad38e6330f3a60d96
Filipinski <140195559+filipinski1@users.noreply.github.com>
2023-12-06 12:43:13 -0500

    Add project files.

---
+++ Models/Settings/TMBDSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace MovieProDemo.Models.Settings
+{
+    public class TMBDSettings
+    {
+        public string BaseUrl{ get; set; }
+        public string BaseImagePath { get;set ; }
+        public string BasrYouTubePath { get; set; }
+        public QueryOptions QueryOptions { get; set; }
+    } 
+    public class QueryOptions
+    {
+        public string Language { get; set; }
+        public string AppendToResponse { get; set; }
+        public string Page { get; set; }
+    }
+}
