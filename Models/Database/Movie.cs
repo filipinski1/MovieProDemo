29b5e8211a31bc5721954425264ec97f681963e1
Filipinski <140195559+filipinski1@users.noreply.github.com>
2023-12-06 14:28:40 -0500

    added classe Movie and its properties

--- Models/Database/Movie.cs
+++ Models/Database/Movie.cs
@@ -1,6 +1,9 @@
 ﻿
+using Microsoft.AspNetCore.Http;
+using MovieProDemo.Enums;
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MovieProDemo.Models.Database
 {
@@ -8,7 +11,7 @@ namespace MovieProDemo.Models.Database
     {
         public int Id { get; set; }
         public int MovieId { get; set; }
-        public string Title { get; set; }   
+        public string Title { get; set; }
         public string TagLine { get; set; }
         public string Overview { get; set; }
         public int RunTime { get; set; }
@@ -18,7 +21,21 @@ namespace MovieProDemo.Models.Database
         public DateTime ReleaseDate { get; set; }
         public MovieRating Rating { get; set; }
 
-            
-           
+        public float VoteAverage { get; set; }
+
+        public byte[] Poster { get; set; }
+        public string PosterType { get; set; }
+
+        public byte[] Backdrop { get; set; }
+        public string BackdropType { get; set; }
+        public string TrailerURL { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Poster Image")]
+        public IFormFile PosterFile { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Backdrop Image")]
+        public IFormFile BackdropFile { get; set; }
     }
 }
