bbecb28f74385778ec4785a12c4d03e33745b3c2
Filipinski <140195559+filipinski1@users.noreply.github.com>
2023-12-08 12:57:24 -0500

    added couple services , did rebuild withour errors

--- Startup.cs
+++ Startup.cs
@@ -45,6 +45,9 @@ namespace MovieProDemo
             services.AddTransient<SeedService>();
             services.AddHttpClient();
             services.AddScoped<IRemoteMovieService, TMDBMovieService>();
+            services.AddScoped<IDataMappingService, TMDBMappingService>();
+            services.AddSingleton<IImageService, BasicImageService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
